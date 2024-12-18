6fa0bf1ad8d49f720776070b8091612fa7925019
Aleksey_Pahalev <83154713+Whynot46@users.noreply.github.com>
2024-12-18 11:20:43 +0400

    add initDB.sql, add populateDB.sql

--- tis_web_app/src/main/resources/database/initDB.sql
+++ tis_web_app/src/main/resources/database/initDB.sql
@@ -1,11 +1,11 @@
 -- Создание таблицы roles
-CREATE TABLE roles (
+CREATE TABLE IF NOT EXISTS roles (
     id SERIAL PRIMARY KEY,  -- Автоинкрементный идентификатор
     name VARCHAR(255) NOT NULL  -- Строка для имени роли
 );
 
 -- Создание таблицы users
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,  -- Автоинкрементный идентификатор
     first_name VARCHAR(255) NOT NULL,  -- Имя пользователя
     patronymic VARCHAR(255),  -- Отчество пользователя
@@ -14,56 +14,56 @@ CREATE TABLE users (
 );
 
 -- Создание таблицы topic
-CREATE TABLE topic (
+CREATE TABLE IF NOT EXISTS topic (
     id SERIAL PRIMARY KEY,  -- Автоинкрементный идентификатор
     name VARCHAR(255) NOT NULL  -- Строка для имени темы
 );
 
 -- Создание таблицы task
-CREATE TABLE task (
+CREATE TABLE IF NOT EXISTS task (
     id SERIAL PRIMARY KEY,  -- Автоинкрементный идентификатор
     question TEXT NOT NULL,  -- Вопрос задачи
     answer TEXT NOT NULL  -- Ответ задачи
 );
 
 -- Создание таблицы test
-CREATE TABLE test (
+CREATE TABLE IF NOT EXISTS test (
     id SERIAL PRIMARY KEY,  -- Автоинкрементный идентификатор
     topic_id INT REFERENCES topic(id),  -- Внешний ключ на таблицу topic
-    tasks_id INT[] REFERENCES task(id),  -- Массив идентификаторов задач
+    tasks_id INT[],  -- Массив идентификаторов задач
     teacher_id INT REFERENCES users(id)  -- Внешний ключ на таблицу users
 );
 
 -- Создание таблицы test_result
-CREATE TABLE test_result (
+CREATE TABLE IF NOT EXISTS test_result (
     id SERIAL PRIMARY KEY,  -- Автоинкрементный идентификатор
     test_id INT REFERENCES test(id),  -- Внешний ключ на таблицу test
     points FLOAT NOT NULL  -- Вещественное число для оценки
 );
 
 -- Создание таблицы admin_profile
-CREATE TABLE admin_profile (
+CREATE TABLE IF NOT EXISTS admin_profile (
     id SERIAL PRIMARY KEY,  -- Автоинкрементный идентификатор
     user_id INT REFERENCES users(id)  -- Внешний ключ на таблицу users
 );
 
 -- Создание таблицы teacher_profile
-CREATE TABLE teacher_profile (
+CREATE TABLE IF NOT EXISTS teacher_profile (
     id SERIAL PRIMARY KEY,  -- Автоинкрементный идентификатор
     user_id INT REFERENCES users(id)  -- Внешний ключ на таблицу users
 );
 
 -- Создание таблицы student_profile
-CREATE TABLE student_profile (
+CREATE TABLE IF NOT EXISTS student_profile (
     id SERIAL PRIMARY KEY,  -- Автоинкрементный идентификатор
     user_id INT REFERENCES users(id),  -- Внешний ключ на таблицу users
-    test_results_id INT[] REFERENCES test_result(id)  -- Массив идентификаторов результатов тестов
+    test_results_id INT[]  -- Массив идентификаторов результатов тестов
 );
 
 -- Создание таблицы class
-CREATE TABLE class (
+CREATE TABLE IF NOT EXISTS class (
     id SERIAL PRIMARY KEY,  -- Автоинкрементный идентификатор
     name VARCHAR(255) NOT NULL,  -- Имя класса
-    students_id INT[] REFERENCES users(id),  -- Массив идентификаторов студентов
+    students_id INT[],  -- Массив идентификаторов студентов
     teacher_id INT REFERENCES users(id)  -- Внешний ключ на таблицу users
 );
