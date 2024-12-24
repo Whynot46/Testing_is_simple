6d7185f2a90245c3af75abd3b8851b5baf7b25ed
Aleksey_Pahalev <83154713+Whynot46@users.noreply.github.com>
2024-12-25 01:23:08 +0400

    delete log

--- tis_web_app/src/main/java/com/tis/tis_web_app/CustomAuthenticationSuccessHandler.java
+++ tis_web_app/src/main/java/com/tis/tis_web_app/CustomAuthenticationSuccessHandler.java
@@ -1,7 +1,5 @@
 package com.tis.tis_web_app;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
@@ -11,15 +9,9 @@ import java.io.IOException;
 
 public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
 
-    private static final Logger logger = LoggerFactory.getLogger(CustomAuthenticationSuccessHandler.class);
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
-        // Логируем информацию о пользователе
-        String username = authentication.getName();
-        String roles = authentication.getAuthorities().toString();
-        logger.info("User {} successfully authenticated with role: {}", username, roles);
-
         // Определяем целевую страницу в зависимости от роли
         String redirectUrl = determineTargetUrl(authentication);
         response.sendRedirect(redirectUrl);
